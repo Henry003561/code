bfbcd32518e78f75e551ec3c72b91dab500afbd3
Henry003561 <hgpfj016@163.com>
2022-08-21 09:31:01 +0800

    code fifth commit

--- c++的基础教学/C++字符串型.cpp
+++ c++的基础教学/C++字符串型.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string> //使用C++风格的字符串的时候要包含的头文件
 using namespace std;
 
 int main()
 {
+    char str[] = "hello world";
+    cout << str << endl;
+    string str2 = "Hello C++";
+    cout << str2 << endl;
+    system("pause");
     return 0;
 }
